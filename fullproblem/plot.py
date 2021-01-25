34f9063001d79dd01624ae00db7c6b52f3828217
Jingmin Xia <jingmin.xia@maths.ox.ac.uk>
2021-01-25 10:41:28 +0800

    Rendering plot of bifurcation with stability

--- fullproblem/plot.py
+++ fullproblem/plot.py
@@ -13,7 +13,7 @@ for functional in ["q2"]:
     def decide_stability(stab):
         print("Deciding on stability of %s" % (stab,))
         return stab[0] == 0
-    dc.bifurcation_diagram(functional, fixed={"c": 1, "xi": 1}, style="ob", unstablestyle="k", decide_stability=decide_stability)
+    dc.bifurcation_diagram(functional, fixed={"c": 1, "xi": 1}, style="ob", unstablestyle="ok", decide_stability=decide_stability)
     plt.xlim((0.2,3.0))
     plt.xticks(numpy.array([0.2,1.0,2.0,3.0]))
     plt.savefig(functional + ".pdf")
